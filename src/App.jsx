eb567a09d4db5031e09353e095b121e615fafebf
crazy-wizardas <wizardas@debian.debian>
2026-03-05 22:12:01 +0200

    searchbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './Search/Search';
+function App() {
+
+  return (
+    <>
+      <Search></Search>
+    </>
+  )
+}
+
+export default App
---
+++ src/Search/Search.jsx
@@ -0,0 +1,25 @@
+import './Search.css';
+import searchGlass from '../IMG/magnifying-glass-solid-full.svg';
+import mic from '../IMG/microphone-solid-full.svg'
+function Search(){
+    return(
+        <>
+            <div className="searchBox">
+                <div className='BoxBox'>
+                    <img src={searchGlass} alt="" className="searchIcon"/>
+                    <div className="searchInputBox">
+                        <input type="text" className="searchInput"/>
+                    </div>
+                    <div className='voiceIconBox'>
+                        <button className='voiceButton'>
+                            <img src={mic} alt="" className="voiceIcon"/>
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Search;
