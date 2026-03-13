edf9d43aa070319489441e7933dbe9a4e9f236c1
crazy-wizardas <pacukasisrusio@gmail.com>
2026-03-13 17:31:33 +0200

    remaking to react native

--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,25 +1,44 @@
 import './Settings.css'
 import Buttons from './Components/Buttons';
+import Plus from '../IMG/addimg.svg'
+import SupportImg from '../IMG/comment-dots-regular-full.svg'
+import SugestionImg from '../IMG/circle-question-regular-full.svg'
+import DarkModeImg from '../IMG/moon-regular-full.svg'
+import { useState } from 'react';
 function Settings() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        document.body.classList.toggle("dark-mode");
+    };
     return (
         <>
             <main className='SettingsMain'>
                 <p className='SettingsName'>Settings</p>
                 <div className='SettingsBox'>
-                    {/* <div className='darkMode'>
-                        <p>Dark mode</p>
-                        <button></button>
-                    </div> */}
                     <button className='BoxButtons'>
+                        <img src={DarkModeImg} alt="" className='SettingImgElement' />
+                        <p>Dark mode</p>
+                        <input
+                            type="checkbox"
+                            checked={darkMode}
+                            onChange={toggleDarkMode}
+                        />
+                    </button>
+                    <button className='BoxButtons'>
+                        <img src={SupportImg} alt="" className='SettingImgElement' />
                         <p>Support</p>
                     </button>
                     <button className='BoxButtons'>
+                        <img src={SugestionImg} alt="" className='SettingImgElement' />
                         <p>Help us improve</p>
                     </button>
                 </div>
                 <div className='SettingsBox'>
-                    <p>For businesses</p>
+                    <p className='BuisnesSettings'>For businesses</p>
                     <button className='BoxButtons'>
+                        <img src={Plus} alt="" className='SettingImgElement' />
                         <p>Add new place</p>
                     </button>
                 </div>
